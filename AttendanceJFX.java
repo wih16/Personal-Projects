678817beecd2369a184acbefa978cbb6b5d11b51
whhinard <williamhhinard@gmail.com>
2016-09-12 09:45:00 -0400

    Adding Attendance

--- AttendanceJFX.java
+++ AttendanceJFX.java
@@ -1,7 +1,10 @@
+
+
 /* Stylistc: 
-Change it so that the grid for the name/email is different than where the email's are added
+
 
 Code:
+
 
  */
 import javafx.application.Application;
@@ -13,12 +16,20 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.geometry.*;
 import java.io.*;
-    
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+
 public class AttendanceJFX extends Application{
     private String ProgramName = " ";
     private Stage stage;
     private File programFile;
-    private int j = 4;
+    private int counter = 1;
+    private File nameFile;
+    private File emailFile;
+    private File programDir;
+
+
+    
     @Override
     public void start(Stage primaryStage){
 	stage = primaryStage;
@@ -45,22 +56,52 @@ public class AttendanceJFX extends Application{
 	TextField ProgramField = new TextField();
 	grid.add(ProgramField, 1, 1);
 
+	ProgramField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			
+		public void handle(KeyEvent event){
+		    if (event.getCode() == KeyCode.ENTER){ 
+			try{
+			    ProgramName = ProgramField.getText();
+			    Boolean dir = new File("Attendance", ProgramName).mkdir();
+			    programDir = new File("Attendance", ProgramName);
+			    programFile = new File(programDir, ProgramName + ".txt");		    
+			    if (!programFile.exists()){
+				programFile.createNewFile();
+			    }
+			    nameFile = new File(programDir, "name.txt");
+			    emailFile = new File(programDir, "email.txt");			
+			}//try
+			catch (IOException ex){
+			    System.out.print("Could not create file");
+			}//catch
+		    
+			stage.setTitle(ProgramName);
+			stage.setScene(Attendance());
+		    }
+		}
+	    });
+	
 	Button ProgramBtn = new Button();
 	ProgramBtn.setText("Enter");
 	ProgramBtn.setOnAction(new EventHandler<ActionEvent>(){
 
 		@Override
 		public void handle(ActionEvent event){
-		    ProgramName = ProgramField.getText();
-		    programFile = new File(ProgramName + ".txt");
 		    try{
+			ProgramName = ProgramField.getText();
+			Boolean dir = new File("Attendance", ProgramName).mkdir();
+			programDir = new File("Attendance", ProgramName);
+			programFile = new File(programDir, ProgramName + ".txt");		    
 			if (!programFile.exists()){
 			    programFile.createNewFile();
 			}
+			nameFile = new File(programDir, "name.txt");
+			emailFile = new File(programDir, "email.txt");			
 		    }//try
 		    catch (IOException ex){
 			System.out.print("Could not create file");
 		    }//catch
+		    
 		    stage.setTitle(ProgramName);
 		    stage.setScene(Attendance());
 		}
@@ -73,7 +114,7 @@ public class AttendanceJFX extends Application{
 
     protected Scene Attendance(){
 	GridPane gridA = new GridPane();
-        gridA.setAlignment(Pos.TOP_CENTER);
+        gridA.setAlignment(Pos.CENTER);
 	gridA.setHgap(5);
 	gridA.setVgap(10);
 	gridA.setPadding(new Insets(10,10,10,10));
@@ -92,57 +133,106 @@ public class AttendanceJFX extends Application{
 	TextField EmailField = new TextField();
 	gridA.add(EmailField, 1, 2);
 
-	Scene scene = new Scene(gridA, 800, 800);
 
-	Button enter = new Button();
-	enter.setText("Enter");
-	enter.setOnAction(new EventHandler<ActionEvent>(){
-
-		@Override
-		public void handle(ActionEvent event){
-		    String name = NameField.getText();
-		    String nameF = String.format("%-10s", name);
-		    String email = EmailField.getText();
-		    String emailF = String.format("%10s", email);
-		    try{
-			FileWriter fw = new FileWriter(programFile, true);
-			PrintWriter output = new PrintWriter(fw);
-			output.println(nameF + "    " + emailF);
-			output.close();
-		    }
-		    catch (IOException e){
-			System.out.println();
-		    }
-		    NameField.setText("");
-		    EmailField.setText("");
-		    Label nameL = new Label(name + ", " + email);
-		    if (j < 19){
-			gridA.add(nameL, 2, j);
-			j++;
-		    }
-		    if (j > 18 && j < 34){
-			int k = j;
-			k -= 15;
-			gridA.add(nameL, 3, k);
-			j++;
-		    }
-		    if (j > 33 && j < 49){
-			int k = j;
-			k -= 30;
-			gridA.add(nameL, 4, k);
-			j++;
-		    }
-		    if (j > 48 && j < 63){
-			int k = j;
-			k -= 45;
-			gridA.add(nameL, 5, k);
-			j++;
-			
-		    }//
-		}
-	    }//event handler
-	    );
+	GridPane gridB = new GridPane();
 	
+	final int numColumns = 3 ;
+        int numRows = 3;
+        for (int i = 0; i < numColumns; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / numColumns);
+            gridB.getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < numRows; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / numRows);
+            gridB.getRowConstraints().add(rowConst);    
+	}
+       
+	gridB.setStyle("-fx-background-color: #CCDBE1;");
+	gridB.add(gridA, 1, 0);
+
+	
+	GridPane gridC = new GridPane();
+
+	numRows = 30;
+	for (int i = 0; i < numColumns; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / numColumns);
+            gridC.getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < numRows; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / numRows);
+            gridC.getRowConstraints().add(rowConst);    
+	}
+
+	gridC.setAlignment(Pos.CENTER);
+	gridC.setStyle("-fc-background-color: #CCDBE1;");
+	gridC.setHgap(5);
+	gridC.setVgap(5);
+	gridC.setPadding(new Insets(10,10,10,10));
+
+	gridB.add(gridC, 0, 1, 3, 2);
+	
+	Scene scene = new Scene(gridB, 800, 600);
+
+
+	EmailField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+		public void handle(KeyEvent event){
+		    if (event.getCode() == KeyCode.ENTER){
+			String name = NameField.getText();
+			String nameF = String.format("%-10s", name);
+			String email = EmailField.getText();
+			String emailF = String.format("%10s", email);
+			try{
+			    //writing to the main file
+			    FileWriter fw = new FileWriter(programFile, true);
+			    PrintWriter output = new PrintWriter(fw);
+			    output.println(nameF + "    " + emailF);
+			    output.close();
+
+			    //writing to the name file
+			    PrintWriter outputName = new PrintWriter(new FileWriter(nameFile, true));
+			    outputName.println(name);
+			    outputName.close();
+
+			    //writing to the email file
+			    PrintWriter outputEmail = new PrintWriter(new FileWriter(emailFile, true));
+			    outputEmail.println(email);
+			    outputEmail.close();			    
+			}
+			catch (IOException e){
+			    System.out.println();
+			}
+			NameField.setText("");
+			EmailField.setText("");
+			Label nameL = new Label(name + ", " + email);
+			int j;
+
+
+			if (counter <= 30){
+			    gridC.add(nameL, 0, counter);
+			    counter++;
+			}
+			else if (counter <= 60){
+			    j = counter - 30;
+			    gridC.add(nameL, 1, j);
+			    counter++;
+			}
+			else if (counter <= 90){
+			    j = counter - 60;
+			    gridC.add(nameL, 2, j);
+			    counter++;
+			}			
+		    }
+		    if (event.getCode() == KeyCode.ESCAPE){
+			System.exit(0);
+		    }
+		}
+	    });
+		
 	Button exit = new Button();
 	exit.setText("Exit");
 	exit.setOnAction(new EventHandler<ActionEvent>(){
@@ -152,9 +242,19 @@ public class AttendanceJFX extends Application{
 		    System.exit(0);
 		}
 	    });
+       
 	
-	gridA.add(enter, 0, 3);
+	EventHandler<KeyEvent> keyExit = new EventHandler<KeyEvent>(){
+		public void handle(KeyEvent event){
+		    if (event.getCode() == KeyCode.ESCAPE){
+			System.exit(0);
+		    }
+		}
+	    };
 
+	NameField.setOnKeyPressed(keyExit);
+	    
+	
 	gridA.add(exit, 1, 3);
 	return scene;
        
@@ -162,3 +262,6 @@ public class AttendanceJFX extends Application{
 
 
 }//public class
+
+
+
